111d3853947211a4f4407832a90c024b3a3ce762
Moctar SAMB <mocsamb@gmail.com>
2021-08-06 11:56:56 +0000

    groups

--- index.js
+++ index.js
@@ -13,16 +13,56 @@ const router = express.Router();
 const instanceId = "29"; // TODO: Replace it with your gateway instance ID here
 const clientId = "moctar.samb@orange-sonatel.com";     // TODO: Replace it with your Forever Green client ID here
 const clientSecret = "add3f7583d0e43beb7b7d250da39bdfc";  // TODO: Replace it with your Forever Green client secret here
-
+const jsonPayload = (message, number) => JSON.stringify({
+    number: number,
+    message: message
+});
+const jsonPayloadGroup = (message, admin, groupname) => JSON.stringify({
+    group_admin: admin, // TODO: Specify the WhatsApp number of the group creator, including the country code
+    group_name: groupname,   // TODO:  Specify the name of the group
+    message: message  // TODO: Specify the content of your message
+});
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
     //__dirname : It will resolve to your project folder.
 });
-router.post('/', function (req, res) {
-    const jsonPayload = (message, number) => JSON.stringify({
-        number: number,
-        message: message
+router.get('/group', function (req, res) {
+    res.sendFile(path.join(__dirname + '/group.html'));
+    //__dirname : It will resolve to your project folder.
+});
+router.post('/group', function (req, res) {
+
+    const options = {
+        hostname: "api.whatsmate.net",
+        port: 80,
+        path: "/v3/whatsapp/single/text/message/" + instanceId,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-WM-CLIENT-ID": clientId,
+            "X-WM-CLIENT-SECRET": clientSecret,
+            "Content-Length": Buffer.byteLength(jsonPayloadGroup(req.body.message, req.body.group_name, req.body.group_name))
+        }
+    };
+
+    const request = new http.ClientRequest(options);
+    request.end(jsonPayloadGroup(req.body.message, req.body.group_admin, req.body.group_name));
+
+    request.on('response', function (response) {
+        console.log(response)
+        console.log('Heard back from the WhatsMate WA Gateway:\n');
+        console.log('Status code: ' + response.statusCode);
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            console.log(chunk);
+        });
+        res.sendFile(path.join(__dirname + '/group.html'));
+        //__dirname : It will resolve to your project folder.
     });
+
+});
+router.post('/', function (req, res) {
+
     console.log("We HEre")
     const options = {
         hostname: "api.whatsmate.net",
@@ -38,23 +78,20 @@ router.post('/', function (req, res) {
     };
 
     const request = new http.ClientRequest(options);
-    console.log((req.body.message+ " "+  req.body.number))
+    console.log((req.body.message + " " + req.body.number))
     request.end(jsonPayload(req.body.message, req.body.number));
 
     request.on('response', function (response) {
-        console.log(response)
         console.log('Heard back from the WhatsMate WA Gateway:\n');
         console.log('Status code: ' + response.statusCode);
         response.setEncoding('utf8');
         response.on('data', function (chunk) {
             console.log(chunk);
         });
-        //__dirname : It will resolve to your project folder.
     });
     res.sendFile(path.join(__dirname + '/index.html'));
 
 });
-//add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
 
